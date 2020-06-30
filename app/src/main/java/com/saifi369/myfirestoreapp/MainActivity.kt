44d564a120d6c0e70bb3f8ee8660c88c8768cc41
saifi369 <chsaif369@gmail.com>
2020-07-01 02:46:33 +0500

    Save & Retrieve multiple documents

--- app/src/main/java/com/saifi369/myfirestoreapp/MainActivity.kt
+++ app/src/main/java/com/saifi369/myfirestoreapp/MainActivity.kt
@@ -5,16 +5,14 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Source
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var firestoreDatabase: FirebaseFirestore
-    private lateinit var userDocumentRef: DocumentReference
+    private lateinit var userCollectionRef: CollectionReference
 
     private val KEY_NAME = "key_name"
     private val KEY_AGE = "key_age"
@@ -24,19 +22,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         firestoreDatabase = FirebaseFirestore.getInstance()
-        userDocumentRef = firestoreDatabase
-            .collection("users").document("person1")
+        userCollectionRef = firestoreDatabase
+            .collection("users")
 
         btn_save_data.setOnClickListener { saveData() }
         btn_read_data.setOnClickListener { readData() }
-        btn_update_data.setOnClickListener { updateData() }
-        btn_delete_person.setOnClickListener { deletePerson() }
-        btn_delete_field.setOnClickListener { deleteName() }
     }
 
     override fun onStart() {
         super.onStart()
-        userDocumentRef.addSnapshotListener(this) { snapshot, e ->
+
+        userCollectionRef.addSnapshotListener(this) { snapshot, e ->
 
             if (e != null) {
                 Toast.makeText(MainActivity@ this, "Error while loading data", Toast.LENGTH_LONG)
@@ -44,12 +40,11 @@ class MainActivity : AppCompatActivity() {
                 Log.d("MyTag", e.localizedMessage)
                 return@addSnapshotListener
             } else {
-                if (snapshot != null && snapshot.exists()) {
-
-                    val person = snapshot.toObject(Person::class.java)
+                tv_output.text = ""
+                for (document in snapshot!!) {
+                    val person = document.toObject(Person::class.java)
+                    person.personId = document.id
                     showOutput(person.toString())
-                } else {
-                    showOutput("Person does not exist")
                 }
             }
         }
@@ -64,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         val person = Person(name, age)
 
-        userDocumentRef.set(person)
+        userCollectionRef.document().set(person)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     showOutput("Data is saved")
@@ -78,62 +73,17 @@ class MainActivity : AppCompatActivity() {
     private fun readData() {
         //this method reads data from firestore
 
-        userDocumentRef.get(Source.DEFAULT)
-
-            .addOnSuccessListener { document ->
-
-                if (document.exists()) {
-
+        userCollectionRef.get()
+            .addOnSuccessListener { querySnapshot ->
+                tv_output.text = ""
+                for (document in querySnapshot) {
                     val person = document.toObject(Person::class.java)
-
+                    person.personId = document.id
                     showOutput(person.toString())
-
-                } else {
-                    showOutput("Person does not exist")
                 }
             }
-            .addOnFailureListener {
-                showOutput(it.localizedMessage)
-            }
-
     }
 
-    private fun updateData() {
-        //this method updates data in firestore
-
-        val name = et_name.text.toString().trim()
-        val age = et_age.text.toString().toInt()
-
-        val data: HashMap<String, Any> = hashMapOf(
-            KEY_NAME to name,
-            KEY_AGE to age
-        )
-
-//        userDocumentRef.set(data, SetOptions.merge())
-
-//        userDocumentRef.update(KEY_NAME,name)
-
-    }
-
-    private fun deleteName() {
-        //this method deletes a single field from any firestore document
-
-//        val data: HashMap<String, Any> = hashMapOf(
-//            KEY_NAME to FieldValue.delete()
-//        )
-//
-//        userDocumentRef.update(data)
-
-        userDocumentRef.update(KEY_NAME, FieldValue.delete())
-
-    }
-
-    private fun deletePerson() {
-        //this method deletes a complete document from firestore database
-
-        userDocumentRef.delete()
-
-    }
 
     private fun showOutput(text: String) {
         if (tv_output.text == "Ready to Learn!")
